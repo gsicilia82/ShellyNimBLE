fff5fdf2c3c5f9c0e24c73c94a03fc5225b15cb6
gsicilia82 <gsicilia82@gmail.com>
2023-03-31 17:45:07 +0200

    Send dist instead of rssi, filtering from dist included

--- src/BleDevice.cpp
+++ src/BleDevice.cpp
@@ -1,27 +1,54 @@
 #include <BleDevice.h>
 
-// private functions
 
-int BleDevice::median_of_3(){
+int BleDevice::get1mRssi(){
 
-    int the_max = max(max(newest, recent), oldest);
-    int the_min = min(min(newest, recent), oldest);
-    // unnecessarily clever code
-    int the_median = the_max ^ the_min ^ newest ^ recent ^ oldest;
-    return (the_median);
+    /*
+    iBeacon: NimBLEBeacon -> int8_t getSignalPower()
+    Allgemein: NimBLEAdvertisedDevice -> bool haveTXPower() -> int8_t getTXPower()
+
+    Über MQTT korrigierbar machen
+    */
+    return -68;
 }
 
-// public functions
 
-BleDevice::BleDevice(){}
+BleDevice::BleDevice() : oneEuro{OneEuroFilter<float, unsigned long>(1, ONE_EURO_FCMIN, ONE_EURO_BETA, ONE_EURO_DCUTOFF)}{}
 
-int BleDevice::getBleValue( NimBLEAdvertisedDevice *advertisedDevice){
+
+bool BleDevice::filter(){
+
+    Reading<float, unsigned long> inter1, inter2;
+    inter1.timestamp = millis();
+    inter1.value = raw;
+
+    return oneEuro.push(&inter1, &inter2) && diffFilter.push(&inter2, &output);
+}
+
+float BleDevice::getBleValue( NimBLEAdvertisedDevice *advertisedDevice){
 
     oldest = recent;
     recent = newest;
     newest = advertisedDevice->getRSSI();
 
-    rssi = median_of_3();
+    int the_max = max(max(newest, recent), oldest);
+    int the_min = min(min(newest, recent), oldest);
+    // unnecessarily clever code
+    int the_median = the_max ^ the_min ^ newest ^ recent ^ oldest;
 
-    return rssi;
+    rssi = the_median;
+
+    float ratio = (get1mRssi() - rssi) / (10.0f * absorption);
+    raw = pow(10, ratio);
+
+    if ( filter() ) hasValue = true;
+
+    #ifdef DEBUG
+        Serial.print( "Device: <" + alias + "> Dist: " + String( raw));
+        if ( hasValue) Serial.println( " | " + String( output.value.position));
+        else Serial.println();
+    #endif
+
+    return hasValue ? output.value.position : raw;
+
 }
--- src/BleDevice.h
+++ src/BleDevice.h
@@ -6,20 +6,29 @@
 #include <globals.h>
 
 #define NO_RSSI (-128)
+#define ONE_EURO_FCMIN 1e-5f
+#define ONE_EURO_BETA 0.2f
+#define ONE_EURO_DCUTOFF 1e-5f
 
 class BleDevice {
 
     private:
         int rssi = NO_RSSI, newest = NO_RSSI, recent = NO_RSSI, oldest = NO_RSSI;
-        float raw = 0;
+        float raw = 0.0f;
+        float absorption = 3.5f;
+        bool hasValue = false;
 
+        Reading<Differential<float>> output;
+
+        OneEuroFilter<float, unsigned long> oneEuro;
+        DifferentialFilter<float, unsigned long> diffFilter;
+
+        int get1mRssi();
         bool filter();
-        int median_of_3();
-
     public:
         String address, alias;
 
         BleDevice();
-        int getBleValue( NimBLEAdvertisedDevice *advertisedDevice);
+        float getBleValue( NimBLEAdvertisedDevice *advertisedDevice);
         
 };
